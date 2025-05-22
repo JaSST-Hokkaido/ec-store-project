af17f2e868f5979e89cd7069b500a34dd2dec303
JaSST-Hokkaido <daisuke.shimoasa@jasst-committee.jp>
2025-05-22 19:53:48 +0900

    デザイン改善: レスポンシブ対応とスタイル修正

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import './styles/index.css';
+import './styles/App.css';
+import './styles/auth.css';
 import App from './App';
 import reportWebVitals from './utils/reportWebVitals';
 import { Amplify } from 'aws-amplify';
